5d48a2b8f95d6378709abbf2de210de27d59a815
Anže Pečar <anze@pecar.me>
2020-12-03 07:12:24 +0000

    Day3

---
+++ src/day3.py
@@ -0,0 +1,39 @@
+import logging
+import math
+from collections import Counter
+
+from utils import get_input
+
+
+grid = """..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#""".splitlines()
+
+
+def slope(step, down):
+    grid = get_input(3)
+    j = 0
+    for i, row in enumerate(grid):
+        if i % down == 1:
+            continue
+        if row[j] == "#":
+            yield 1
+        j = j + step
+        j = j % len(row)
+
+
+def run():
+    logging.info(sum((slope(3, 1))))
+    logging.info(
+        math.prod(
+            [sum(slope(s, d)) for s, d in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]
+        )
+    )
