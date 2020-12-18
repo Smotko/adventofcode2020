7f936bfb4442e82e26af83f0d20ebc7269955f80
Anže Pečar <anze@pecar.me>
2020-12-18 08:47:42 +0000

    Solve p2

--- src/day18.py
+++ src/day18.py
@@ -1,6 +1,6 @@
 from .utils import *
 import ply.lex as lex
-
+import ply.yacc as yacc
 
 tokens = (
     "NUMBER",
@@ -37,53 +37,79 @@ def t_error(t):
 lexer = lex.lex()
 
 
-import ply.yacc as yacc
+def compute1(inp):
+    def p_expression_plus(p):
+        "expression : expression PLUS factor"
+        p[0] = p[1] + p[3]
 
-# Get the token map from the lexer.  This is required.
+    def p_expression_minus(p):
+        "expression : expression TIMES factor"
+        p[0] = p[1] * p[3]
 
+    def p_expression_term(p):
+        "expression : factor"
+        p[0] = p[1]
 
-def p_expression_plus(p):
-    "expression : expression PLUS factor"
-    p[0] = p[1] + p[3]
+    def p_factor_num(p):
+        "factor : NUMBER"
+        p[0] = p[1]
 
+    def p_factor_expr(p):
+        "factor : LPAREN expression RPAREN"
+        p[0] = p[2]
 
-def p_expression_minus(p):
-    "expression : expression TIMES factor"
-    p[0] = p[1] * p[3]
+    # Error rule for syntax errors
+    def p_error(p):
+        print("Syntax error in input!")
 
-
-def p_expression_term(p):
-    "expression : factor"
-    p[0] = p[1]
-
-
-def p_factor_num(p):
-    "factor : NUMBER"
-    p[0] = p[1]
-
-
-def p_factor_expr(p):
-    "factor : LPAREN expression RPAREN"
-    p[0] = p[2]
-
-
-# Error rule for syntax errors
-def p_error(p):
-    print("Syntax error in input!")
-
-
-# Build the parser
-parser = yacc.yacc()
-
-
-def compute(inp):
+    # Build the parser
+    parser = yacc.yacc()
     lexer.input(inp)
     result = parser.parse(inp)
-    info(result)
+    return result
+
+
+def compute2(inp):
+    def p_expression_plus(p):
+        "expression : expression TIMES term"
+        p[0] = p[1] * p[3]
+
+    def p_expression_term(p):
+        "expression : term"
+        p[0] = p[1]
+
+    def p_term_times(p):
+        "term : term PLUS factor"
+        p[0] = p[1] + p[3]
+
+    def p_term_factor(p):
+        "term : factor"
+        p[0] = p[1]
+
+    def p_factor_num(p):
+        "factor : NUMBER"
+        p[0] = p[1]
+
+    def p_factor_expr(p):
+        "factor : LPAREN expression RPAREN"
+        p[0] = p[2]
+
+    # Error rule for syntax errors
+    def p_error(p):
+        print("Syntax error in input!")
+
+    # Build the parser
+    parser = yacc.yacc()
+    lexer.input(inp)
+    result = parser.parse(inp)
     return result
 
 
 def run():
-    assert compute("1 + 2 * 3 + 4 * 5 + 6") == 71
-    assert compute("1 + (2 * 3) + (4 * (5 + 6))") == 51
-    info(sum(compute(i) for i in get_input(18)))
+    assert compute1("1 + 2 * 3 + 4 * 5 + 6") == 71
+    info(sum(compute1(i) for i in get_input(18)))
+    # yacc really doesn't like being parsed twice in a single file
+    # so only one function can be run at once
+    # assert compute2("1 + 2 * 3 + 4 * 5 + 6") == 231
+    # assert compute2("1 + (2 * 3) + (4 * (5 + 6))") == 51
+    # info(sum(compute2(i) for i in get_input(18)))
